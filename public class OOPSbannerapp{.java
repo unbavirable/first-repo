7fde1ddfc3c50b81264dc77fa2c1c9a129f45d0c
unbavirable <bglikescoding@gmail.com>
2026-03-17 17:50:26 +0530

    code for uc2

---
+++ public class OOPSbannerapp{.java
@@ -0,0 +1,15 @@
+public class OOPSbannerapp{
+
+    public static void main(String[] args) {
+
+        System.out.println("   *        *      ***       ***   ");
+        System.out.println(" *   *    *   *    *    *     **       ");
+        System.out.println("*     *  *     *   *     *   **        ");
+        System.out.println("*     *  *     *   *    *     **        ");
+        System.out.println("*     *  *     *   **         *     ");
+        System.out.println("*     *  *     *   *                *     ");
+		System.out.println("*		* *     *   *                 *     ");
+        System.out.println(" *   *    *   *    *                *		");
+        System.out.println("   *        *      *           ***   ");
+    }
+}
